15b4a76d793eaedd62df3e16255c8232dc505d2f
Torrence Cole <trcole@gmail.com>
2021-11-24 10:19:31 -0500

    setup testing on main package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,32 +21,10 @@ var session *scs.SessionManager
 
 // The main application function
 func main() {
-	// What am I going to put in the session
-	gob.Register(models.Reservation{})
-
-	// change this to true in production
-	app.InProduction = false
-
-	session = scs.New()
-	session.Lifetime = 24 * time.Hour
-	session.Cookie.Persist = true
-	session.Cookie.SameSite = http.SameSiteLaxMode
-	session.Cookie.Secure = app.InProduction
-
-	app.Session = session
-
-	tc, err := render.CreateTemplateCache()
-	if err !=nil {
-		log.Fatal("cannot create template cache")
+	err := run()
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	app.TemplateCache = tc
-	app.UseCache = false
-
-	repo := handlers.NewRepo(&app)
-	handlers.NewHandlers(repo)
-
-	render.NewTemplates(&app)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
@@ -57,4 +35,36 @@ func main() {
 
 	err = srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func run() error {
+		// What am I going to put in the session
+		gob.Register(models.Reservation{})
+
+		// change this to true in production
+		app.InProduction = false
+	
+		session = scs.New()
+		session.Lifetime = 24 * time.Hour
+		session.Cookie.Persist = true
+		session.Cookie.SameSite = http.SameSiteLaxMode
+		session.Cookie.Secure = app.InProduction
+	
+		app.Session = session
+	
+		tc, err := render.CreateTemplateCache()
+		if err !=nil {
+			log.Fatal("cannot create template cache")
+			return err
+		}
+	
+		app.TemplateCache = tc
+		app.UseCache = false
+	
+		repo := handlers.NewRepo(&app)
+		handlers.NewHandlers(repo)
+	
+		render.NewTemplates(&app)
+
+	return nil
 }
