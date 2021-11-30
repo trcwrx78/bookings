4f719b3152863db038e436568d6fe0efae6b2ca7
Torrence Cole <trcole@gmail.com>
2021-11-30 10:45:38 -0500

    Improved error handling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/trcwrx78/bookings/internal/config"
 	"github.com/trcwrx78/bookings/internal/handlers"
+	"github.com/trcwrx78/bookings/internal/helpers"
 	"github.com/trcwrx78/bookings/internal/models"
 	"github.com/trcwrx78/bookings/internal/render"
 
@@ -18,6 +20,8 @@ import (
 const portNumber = ":8080"
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // The main application function
 func main() {
@@ -43,7 +47,13 @@ func run() error {
 
 		// change this to true in production
 		app.InProduction = false
-	
+
+		infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+		app.InfoLog = infoLog
+
+		errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+		app.ErrorLog = errorLog
+
 		session = scs.New()
 		session.Lifetime = 24 * time.Hour
 		session.Cookie.Persist = true
@@ -63,8 +73,9 @@ func run() error {
 	
 		repo := handlers.NewRepo(&app)
 		handlers.NewHandlers(repo)
-	
 		render.NewTemplates(&app)
+		helpers.NewHelpers(&app)
+	
 
 	return nil
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -3,11 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/trcwrx78/bookings/internal/config"
 	"github.com/trcwrx78/bookings/internal/forms"
+	"github.com/trcwrx78/bookings/internal/helpers"
 	"github.com/trcwrx78/bookings/internal/models"
 	"github.com/trcwrx78/bookings/internal/render"
 )
@@ -34,25 +34,13 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
-
 	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	// Perform some logic
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello again."
-
-	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
-	stringMap["remote_ip"] = remoteIP
-
 	// send the data
-	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{
-		StringMap: stringMap,
-	})
+	render.RenderTemplate(w, r, "about.page.tmpl", &models.TemplateData{})
 }
 
 // Renders the make a reservation page
@@ -71,7 +59,7 @@ func(m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 func(m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
 		return
 	}
 
@@ -85,7 +73,7 @@ func(m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	form := forms.New(r.PostForm)
 
 	form.Required("first_name", "last_name", "email")
-	form.MinLength("first_name", 3, r)
+	form.MinLength("first_name", 3)
 	form.IsEmail("email")
 
 	if !form.Valid() {
@@ -144,7 +132,8 @@ func(m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 
 	out, err := json.MarshalIndent(resp, "", "     ")
 	if err != nil {
-		log.Println(err)
+		helpers.ServerError(w, err)
+		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -160,7 +149,7 @@ func(m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 func(m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
 	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
-		log.Println("Cannot get item from session")
+		m.App.ErrorLog.Println("Can't get error from session")
 		m.App.Session.Put(r.Context(), "error", "Can't get reservation from session")
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
